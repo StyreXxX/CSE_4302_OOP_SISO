bb43f2a8be18c4fc688ab316f9b79af4d574b4af
StyreXxX <rayhan37@iut-dhaka.edu>
2026-03-08 14:16:14 +0600

    Niloy ekhane theke shuru kor or ei fiel palta

---
+++ earthquake_prediction.cpp
@@ -0,0 +1,6 @@
+#include "earthquake_prediction.h"
+#include <iostream>
+
+void EarthquakePrediction::run() {
+    std::cout << "\n[Module 3: Earthquake Prediction - Under Construction by Teammate]\n";
+}
