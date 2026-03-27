17fb0b6950f2f7f8c6e7db64cf6f35b4d48f8049
StyreXxX <rayhan37@iut-dhaka.edu>
2026-03-27 16:06:28 +0600

    Deleted useless files

--- earthquake_prediction.cpp
+++
@@ -1,6 +0,0 @@
-#include "earthquake_prediction.h"
-#include <iostream>
-
-void EarthquakePrediction::run() {
-    std::cout << "\n[Module 3: Earthquake Prediction - Under Construction by Teammate]\n";
-}
