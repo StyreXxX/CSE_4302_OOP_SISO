837d2fd1981ba6021fdf11e6567b3aeea9aa79e3
saminrayed <saminrayed0101@gmail.com>
2026-03-27 16:36:18 +0600

    Add country risk model headers
    Added derived risk model class declarations for supported countries and model factory function.

---
+++ CountryRisks.h
@@ -0,0 +1,78 @@
+#ifndef COUNTRYRISKS_H
+#define COUNTRYRISKS_H
+
+#include "RiskModel.h"
+
+class BangladeshRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class JapanRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class USARisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class TurkeyRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class NepalRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class IndiaRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class IndonesiaRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class MexicoRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class ChileRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+class ItalyRisk : public RiskModel {
+public:
+    double calculateRisk(const Building& b) const override;
+    string getCountryName() const override;
+    void showRules() const override;
+};
+
+RiskModel* createCountryModel(int choice);
+
+#endif
